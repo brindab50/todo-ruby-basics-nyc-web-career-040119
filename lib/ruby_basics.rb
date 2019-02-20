484245e999f259b1dda06873cd33975ae4501d10
brindab50 behal <brindab50@gmail.com>
2019-02-20 19:37:46 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -3,23 +3,24 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-  
+  var = name 
+  return "name"
 end
 
-def argue
-  
+def argue(arguement)
+  return ""
 end
 
-def greeting
-
+def greeting(greeting, name)
+return "greeting #{name}"
 end
 
 def return_a_value
-
+return "Nice"
 end
 
 def last_evaluated_value
-
+return "expert"
 end
 
 def pizza_party
